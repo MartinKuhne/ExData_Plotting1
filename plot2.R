345d6fd04f03c86293fca60a7a4afe861d71b328
MartinKuhne <mkuhne@outlook.com>
2014-10-09 16:51:34 -0700

    plot 2

---
+++ plot2.R
@@ -0,0 +1,24 @@
+# plot2.R
+# Author: Martin Kuhne
+# Summary: plot for week 1 assignment
+# cf. https://class.coursera.org/exdata-007/
+
+source("data.R")
+library("lubridate") # for parse_date_time
+
+data <- getEnergyData()
+
+# convert date and time from string to date format
+data <- mutate(data, dateTime = parse_date_time(paste(Date, Time, sep = " "), "%d%m%y %H%M%S"))
+
+png(file = "plot2.png")
+
+with(data, 
+ {
+     plot(dateTime, GlobalActivePower, type="n", 
+          ylab = "Global Active Power (kilowatts)",
+          xlab = "")
+     lines(dateTime, GlobalActivePower)
+ })
+
+dev.off()
